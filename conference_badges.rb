097916f5b6cfb10fb56480e3adb42e50cec4ebe0
Nick Scofield <c.nick.scofield@gmail.com>
2023-02-02 13:50:50 -0700

    commit tree c1fabc0489db067007b752f83906f7e1e3d7674a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map { |attendee| badge_maker(attendee) }
+end
+
+def assign_rooms(attendees)
+  attendees.map.with_index(1) do |attendee, room_num|
+    "Hello, #{attendee}! You'll be assigned to room #{room_num}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each { |badge| puts badge }
+  assign_rooms(attendees).each { |room_assignment| puts room_assignment }
+end
